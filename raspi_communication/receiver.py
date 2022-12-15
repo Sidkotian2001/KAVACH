24e91132cd04cc936756c41d67f23ddb02449ae1
Siddharth Kotian <siddharthkotian3112@gmail.com>
2022-12-15 14:31:02 +0530

    Merge changes but do not use

--- raspi_communication/receiver.py
+++ raspi_communication/receiver.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import socket
 import struct
-from iris_local_kivy import iris_voice
 
 MAX_DGRAM = 2**16
 
@@ -19,28 +18,26 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('192.168.237.170', 12348))
     dat = b''
+<<<<<<< HEAD
     print('Enter dump buffer')
     # dump_buffer(s)
     print('Exit dump buffer')
     iris_obj = iris_voice()
     i = 0
+=======
+    dump_buffer(s)
+>>>>>>> parent of 3c9b8d4... integrated gui with mediapipe
     while True:
         seg, addr = s.recvfrom(MAX_DGRAM)
         if struct.unpack('B', seg[0:1])[0] > 1:
             dat += seg[1:]
         else:
             dat += seg[1:]
-            img = cv2.imdecode(np.frombuffer(dat, dtype=np.uint8), 1)
-            number_of_eyes_captured = 0
-            if i == 300:
-                number_of_eyes_captured += 1
-                i = 0
-            new_frame = iris_obj.capture(img, number_of_eyes_captured)
-            cv2.imshow('frame', new_frame)
+            img = cv2.imdecode(np.fromstring(dat, dtype=np.uint8), 1)
+            cv2.imshow('frame', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             dat = b''
-        i += 1
     # cap.release()
     cv2.destroyAllWindows()
     s.close()
