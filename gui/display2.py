c84c9f846e884760ef9cc5c0d78e33ff8e341268
Ayush-Batra <Ayush-Batra>
2022-12-16 10:28:53 +0530

    button added

--- gui/display2.py
+++ gui/display2.py
@@ -10,6 +10,7 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
@@ -22,7 +23,14 @@ import cv2
 from iris_local_kivy import iris_voice
 import os
 
+#Make background color white
 Window.clearcolor = (1,1,1,1)
+
+
+# class for managing screens
+class WindowManager(ScreenManager):
+	def __init__(self, **kwargs):
+		super(WindowManager, self).__init__(**kwargs)
 
 #class for running model
 class runningWindow(Screen):
@@ -73,7 +81,7 @@ class loginWindow(Screen):
 		# 	else:
 
 		# 		popFun()
-		sm.current = 'logdata'
+		sm.current = 'patient_info'
 			
 
 
@@ -110,140 +118,141 @@ class signupWindow(Screen):
 # class to display validation result
 class logDataWindow(Screen):
 	def runbtn(self):
-		os.system('../models/detect.py')
+		print("running")
+		os.system('python3 ../models/detect.py')
 	pass
 
-# class for managing screens
-class WindowManager(ScreenManager):
-	pass
 
+
+#Class for capturing the images from video feed
 class VideoCapture(Screen):
-	def __init__(self, **kwargs):
-		super(VideoCapture, self).__init__(**kwargs)
-		
-		self.texture = None
-		self.iris_obj = None
-		self.number_of_eyes_captured = 0
-		self.is_eye_in_square = False
-		self.frame_original = None
-        # self.layout = FloatLayout(
-		self.img1 = Image(size_hint = (.96, .72),
+    def __init__(self, **kwargs):
+        super(VideoCapture, self).__init__(**kwargs)
+        # Screen.__init__(self, **kwargs)
+
+        self.iris_obj = None
+        self.number_of_eyes_captured = 0
+        self.is_eye_in_square = False
+        self.frame_original = None
+        
+        self.img1 = Image(size_hint = (.96, .72),
                         pos_hint = {'center_x' : .5, 'center_y': .60}
                         )
         
         #Button 0
-		self.button0 = Button(text = "Start video",
+        self.button0 = Button(text = "Start video",
                         size_hint = (0.15, 0.1),
                         pos_hint = {'center_x' : .25, 'center_y': .15},
-                        disabled = False
+                        disabled = False,
+                        on_release = self.start_video
                         )
-		self.button0.bind(on_press = self.start_video)
 
         #Button 1
-		self.button1 = Button(text = "Capture",
-					size_hint = (0.15, 0.1),
-					pos_hint = {'center_x' : .50, 'center_y': .15},
-					disabled = True
-					)
-		self.button1.bind(on_press = self.save_img)
+        self.button1 = Button(text = "Capture",
+                    size_hint = (0.15, 0.1),
+                    pos_hint = {'center_x' : .50, 'center_y': .15},
+                    disabled = True,
+                    on_release = self.save_img
+                    )
 
-		#Button 2
-		self.button2 = Button(text = "Flash",
-					size_hint = (0.15, 0.1),
-					pos_hint = {'center_x' : .75, 'center_y': .15},
-					disabled = True
-					)
-		self.button3 = Button(text = "Next",
+        #Button 2
+        self.button2 = Button(text = "Flash",
+                    size_hint = (0.15, 0.1),
+                    pos_hint = {'center_x' : .75, 'center_y': .15},
+                    disabled = True,
+                    on_release = self.change_illumination
+                    )
+
+        self.button3 = Button(text = "Next",
                     size_hint = (0.15, 0.1),
                     pos_hint = {'center_x' : .75, 'center_y': .05},
                     disabled = True,
                     #change to next screen
                     on_release = self.change_screen
                     )
-		# self.button2.bind(on_press = self.change_illumination)
-		self.iris_obj = iris_voice()
-		self.add_widget(self.img1)
-		self.add_widget(self.button0)
-		self.add_widget(self.button1)
-		self.add_widget(self.button2)
-		self.add_widget(self.button3)
-		self.clock_schedule()
 
-	def change_screen(self,_):
+        self.iris_obj = iris_voice()
+        self.add_widget(self.img1)
+        self.add_widget(self.button0)
+        self.add_widget(self.button1)
+        self.add_widget(self.button2)
+        self.add_widget(self.button3)
+        self.clock_schedule()
+    def change_screen(self,_):
         #cdestroy the camera object
-		self.button0.disabled = False
-		del self.iris_obj
-		sm.current = 'logdata'
-	def clock_schedule(self):
-		Clock.schedule_interval(self.update, 1.0/33.0)
+        # del self.iris_obj
+        sm.current = 'logdata'
+    def clock_schedule(self):
+        Clock.schedule_interval(self.update, 1.0/33.0)
 
-	def update(self, _):
+    def update(self, _):
+        if self.button0.disabled == True:
+            try:
+                frame = self.iris_obj.capture(self.number_of_eyes_captured)
+                self.frame_original = self.iris_obj.frame_original
+                self.is_eye_in_square = self.iris_obj.is_eye_in_square
+                frame = cv2.flip(frame, 0)
 
-		if self.button0.disabled == True:
-			
-			frame = self.iris_obj.capture(self.number_of_eyes_captured)
-			self.frame_original = self.iris_obj.frame_original
-			self.is_eye_in_square = self.iris_obj.is_eye_in_square
-			frame = cv2.flip(frame, 0)	
-			
+                buf = frame.tobytes()
+                
+                texture = Texture.create(size = (640, 480), 
+                                colorfmt = 'bgr')
+                #if working on RASPBERRY PI, use colorfmt='rgba' here instead, but stick with "bgr" in blit_buffer. 
+                
+                texture.blit_buffer(buf, colorfmt = 'bgr', bufferfmt = 'ubyte')
 
-			buf = frame.tobytes()
-			
-			self.texture = Texture.create(size = (640, 480), 
-							colorfmt = 'bgr')
-			#if working on RASPBERRY PI, use colorfmt='rgba' here instead, but stick with "bgr" in blit_buffer. 
-			
-			self.texture.blit_buffer(buf, colorfmt = 'bgr', bufferfmt = 'ubyte')
+                self.img1.texture = texture
+            except:
+                pass
+            
+        else:
+            self.img1.source = 'camera_icon.png'
 
-			self.img1.texture = self.texture
+    def start_video(self, _):
+        self.button0.disabled = True
+        self.button1.disabled = False
+        self.button2.disabled = False
+        self.button3.disabled = False
 
-			
-		else:
-			self.img1.source = 'camera_icon.png'
+    def save_img(self, _):
+        if self.is_eye_in_square == True:
+            cv2.imwrite('image_taken_{}.png'.format(str(self.number_of_eyes_captured)), self.frame_original)
+            self.number_of_eyes_captured += 1
+            if self.number_of_eyes_captured > 1:
+                self.next_page()
+        else:
+            pass
 
-	def start_video(self, _):
-		self.button0.disabled = True
-		self.button1.disabled = False
-		self.button2.disabled = False
-		self.button3.disabled = False
-
-	def save_img(self, _):
-		if self.is_eye_in_square == True:
-			cv2.imwrite('image_taken_{}.png'.format(str(self.number_of_eyes_captured)), self.frame_original)
-			self.number_of_eyes_captured += 1
-			if self.number_of_eyes_captured > 1:
-				self.next_page()
-		else:
-			pass
-
-	def change_illumination(self, _):
-		print("This button will adjust the illumination")
+    def change_illumination(self, _):
+        print("This button will adjust the illumination")
 
 
 
-
+#Class for viewing the captured images
 class View_Images(Screen):
 	pass
 
+#Class to display patient information
 class GetPatientInfo(Screen):
 	pass
+
+
 
 # kv file
 kv = Builder.load_file('login.kv')
 sm = WindowManager()
-
-
-
-# adding screens
-sm.add_widget(loginWindow(name='login'))
-sm.add_widget(signupWindow(name='signup'))
-sm.add_widget(logDataWindow(name='logdata'))
-sm.add_widget(runningWindow(name='running'))
-sm.add_widget(VideoCapture(name='videofeed'))
-sm.add_widget(View_Images(name = 'view_images'))
+		
 # class that builds gui
 class loginMain(App):
 	def build(self):
+		# adding screens
+		sm.add_widget(loginWindow(name='login'))
+		sm.add_widget(signupWindow(name='signup'))
+		sm.add_widget(logDataWindow(name='logdata'))
+		sm.add_widget(runningWindow(name='running'))
+		sm.add_widget(VideoCapture(name='video'))
+		sm.add_widget(View_Images(name = 'view_images'))
+		sm.add_widget(GetPatientInfo(name = 'patient_info'))
 		return sm
 
 # driver function
