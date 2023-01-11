37b5bb0f91f95c6bf8f929d5a4b55410f36e5ab5
Siddharth Kotian <siddharthkotian3112@gmail.com>
2023-01-11 18:09:25 +0530

    first page done

--- newgui/newgui.py
+++ newgui/newgui.py
@@ -6,18 +6,45 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.effectwidget import AdvancedEffectBase, EffectWidget
 from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
+
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.graphics import Rectangle, Color
 
-Window.size = (755, 480)
-Window.clearcolor = (0,0.267,0.4,1)
+
+
+Window.size = (940, 600)
+# Window.clearcolor = (0,0.267,0.4,1)
+Window.clearcolor = (0,0,0,1)
 
 class WindowManager(ScreenManager):
     pass
 
+p_fn = ''
+p_ln = ''
+p_m = ''
+p_a = ''
+p_g = ''
+
+
+
 class patientWindow(Screen):
-    pass
+    patient_firstname = ObjectProperty(None)
+    patient_lastname = ObjectProperty(None)
+    patient_mobile = ObjectProperty(None)
+    patient_age = ObjectProperty(None)
+    patient_gender = ObjectProperty(None)
+
+    def submit_info(self):
+        globals()['p_fn'] = self.patient_firstname.text
+        globals()['p_ln'] = self.patient_lastname.text
+        globals()['p_m'] = self.patient_mobile.text
+        globals()['p_a'] = self.patient_age.text
+        globals()['p_g'] = self.patient_gender.text
+        print(globals()['p_fn'])
+        print(globals()['p_g'])
 
 kv = Builder.load_file('components.kv')
 sm = WindowManager()
